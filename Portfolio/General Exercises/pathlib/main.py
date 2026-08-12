8a1504182b3155c0db13268af1f683e0f7107b8c
Bence <bblasika@gmail.com>
2026-08-12 15:44:16 +0200

    Started to group files by extension, not finished yet

--- Portfolio/General Exercises/pathlib/main.py
+++ Portfolio/General Exercises/pathlib/main.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
-p1 = Path(r"C:\Users\bblas\OneDrive\Pictures")
-files = list(p1.iterdir())
+# Listing every file in a directory
+
+p1 = Path(r"S:\Elektronik\E2\E2V1\E2V1P\E2V1P_Products\NEDAP\07_AV\02_BoxBuild\02_PxN")
+files = [p for p in p1.rglob("*") if p.is_file()]
+
+# grouping files by extensions
+
+grouped_files = {}
 
 for file in files:
-    print(file)
+    grouped_files[file.suffix] = file.stem
 
-# Listing every file in a directory
+print(grouped_files)
