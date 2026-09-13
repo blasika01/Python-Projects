4f5b964be0fb1c6ba87efddbda2ca3175e9d5597
blasika01 <bblasika@gmail.com>
2026-09-13 19:28:09 +0200

    Creted a basic login and register form

--- Portfolio/GeneralExercises/flask/app.py
+++ Portfolio/GeneralExercises/flask/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "0a5ac5c5713a2f456aa50247e5afa55b"
 
 posts = [
     {
@@ -26,6 +29,24 @@ def home():
 def about():
     return render_template("about.html", title="About")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Registration", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Failed. Please check username and password", "danger")
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
