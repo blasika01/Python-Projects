0baa0835d221a9ee0e5fa15ff20cc94930c305c2
blasika01 <bblasika@gmail.com>
2026-09-21 18:00:34 +0200

    Added a sql database for the code

--- Portfolio/GeneralExercises/flask/app.py
+++ Portfolio/GeneralExercises/flask/app.py
@@ -1,9 +1,40 @@
+from datetime import datetime ,timezone
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "0a5ac5c5713a2f456aa50247e5afa55b"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(db.String(20), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(db.String(120), unique=True, nullable=False)
+    image_file: Mapped[str] = mapped_column(db.String(20), nullable=False, default="default.jpg")
+    password: Mapped[str] = mapped_column(db.String(60), nullable=False)
+    posts: Mapped[List["Post"]] = relationship(backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(db.String(100), nullable=False)
+    date_posted: Mapped[datetime] = mapped_column(nullable=False, default=lambda: datetime.now(timezone.utc))
+    content: Mapped[str] = mapped_column(db.Text, nullable=False)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
